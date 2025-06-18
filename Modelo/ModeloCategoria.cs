cf9bbe11674dd35b66e550a262d5f54805e29207
Bueno-Italo <italo-bueno@hotmail.com>
2025-06-18 09:41:39 -0300

    classe para o DB

--- Modelo/ModeloCategoria.cs
+++ Modelo/ModeloCategoria.cs
@@ -9,10 +9,16 @@ namespace Modelo
     public class ModeloCategoria
     {
         private int cat_cod;
-        public int Cat_Cod
-        { 
+        public int CatCod
+        {
             get { return this.cat_cod; }
             set { this.cat_cod = value; }
+        }
+        private String cat_nome;
+        public String CatNome
+        {
+            get { return this.cat_nome; }
+            set { this.cat_nome = value; }
         }
     }
 }
