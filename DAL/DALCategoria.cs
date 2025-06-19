f40c8caa86a9a781c825325f8ea16395c8956773
Bueno-Italo <italo-bueno@hotmail.com>
2025-06-19 17:24:28 -0300

    Finalisando principais metodos e classe DALCategoria

--- DAL/DALCategoria.cs
+++ DAL/DALCategoria.cs
@@ -1,8 +1,10 @@
 ﻿using Modelo;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,10 +37,53 @@ namespace DAL
             cmd.Connection = conexao.ObjetoConexao;
             cmd.CommandText = "update categoria set cat_nome = @nome where cat_cod = @codigo;";
             cmd.Parameters.AddWithValue("@nome", modelo.CatNome);
-            cmd.Parameters.AddWithValue("@codigo", modelo.CatCod); 
+            cmd.Parameters.AddWithValue("@codigo", modelo.CatCod);
             conexao.Conectar();
             cmd.ExecuteNonQuery();
             conexao.Desconectar();
+        }
+
+        //Excluir Categoria
+        public void Excluir(int codigo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "delete from categoria where cat_cod = @codigo;";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+        //Localizar categoria com base em um valor
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from categoria where cat_nome like '%" +
+               valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        //Informações de um registro no DB e preencher objeto
+        public ModeloCategoria CarregaModeloCategoria(int codigo)
+        {
+            ModeloCategoria modelo = new ModeloCategoria();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "select * from contato where con_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.CatCod = Convert.ToInt32(registro["cat_cod"]);
+                modelo.CatNome = Convert.ToString(registro["cat_nome"]);
+            }
+            conexao.Desconectar();
+            return modelo;
+
         }
     }
 }
