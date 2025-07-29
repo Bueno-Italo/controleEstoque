0d67d5fce74b446dd0e112ac705bb8cdd1c97792
Bueno-Italo <italo-bueno@hotmail.com>
2025-07-29 09:59:31 -0300

    Implementando classe DAL Unidade de Medida

---
+++ DAL/DALUnidadeDeMedida.cs
@@ -0,0 +1,117 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALUnidadeDeMedida
+    {
+        private DALConexao conexao;
+        public DALUnidadeDeMedida(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloUnidadeDeMedida modelo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "insert into undmedida(umed_nome) values (@nome); select @@IDENTITY;";
+                cmd.Parameters.AddWithValue("@nome", modelo.UmedNome);
+                conexao.Conectar();
+                modelo.UmedCod = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        // Alterar nome da categoria com base no código
+        public void Alterar(ModeloUnidadeDeMedida modelo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "update undmedida umed_nome = @nome where umed_cod = @cod;";
+                cmd.Parameters.AddWithValue("@nome", modelo.UmedNome);
+                cmd.Parameters.AddWithValue("@cod", modelo.UmedCod);
+                conexao.Conectar();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        //Excluir Categoria
+        public void Excluir(int codigo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "delete from undmedida where umed_cod = @codigo;";
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                conexao.Conectar();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        //Localizar categoria com base em um valor
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("select * from undmedida  where umed_nome like '%" +
+                valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        //Informações de um registro no DB e preencher objeto
+        public ModeloUnidadeDeMedida CarregaModeloSubCategoria(int codigo)
+        {
+            ModeloUnidadeDeMedida modelo = new ModeloUnidadeDeMedida();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "select * from undmedida where umed_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.UmedCod = Convert.ToInt32(registro["umed_cod"]);
+                modelo.UmedNome = Convert.ToString(registro["umed_nome"]);
+            }
+            conexao.Desconectar();
+            return modelo;
+        }
+    }
+}
