fc353d63725f6ff618bc1270c27b872369f0a6e5
Bueno-Italo <italo-bueno@hotmail.com>
2025-09-01 22:02:33 -0300

    Implementando a classe DALProduto

---
+++ DAL/DALProduto.cs
@@ -0,0 +1,52 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALProduto
+    {
+        private DALConexao conexao;
+
+        public DALProduto(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloProduto obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "insert into Produto (pro_nome, pro_descricao, pro_foto,  pro_valorpago, pro_valorvenda, pro_qtde,umed_cod , cat_cod, scat_cod) " +
+            "values (@nome,@descricao,@foto,@valorpago,@valorvenda,@qtde,@undmedcod,@catcod,@scatcod); select @@IDENTITY;";
+            cmd.Parameters.AddWithValue("@nome", obj.ProNome);
+            cmd.Parameters.AddWithValue("@descricao", obj.ProDescricao);
+            cmd.Parameters.Add("@foto", System.Data.SqlDbType.Image);
+            if (obj.ProFoto == null)
+            {
+                //cmd.Parameters.AddWithValue("@pro_foto", DBNull.Value);
+                cmd.Parameters["@foto"].Value = DBNull.Value;
+            }
+            else
+            {
+                //cmd.Parameters.AddWithValue("@pro_foto", obj.pro_foto);
+                cmd.Parameters["@foto"].Value = obj.ProFoto;
+            }
+            cmd.Parameters.AddWithValue("@valorpago", obj.ProValorPago);
+            cmd.Parameters.AddWithValue("@valorvenda", obj.ProValorVenda);
+            cmd.Parameters.AddWithValue("@qtde", obj.ProQtde);
+            cmd.Parameters.AddWithValue("@undmedcod", obj.UmedCod);
+            cmd.Parameters.AddWithValue("@catcod", obj.CatCod);
+            cmd.Parameters.AddWithValue("@scatcod", obj.ScatCod);
+            conexao.Conectar();
+            obj.ProCod = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconectar();
+        }
+
+    }
+
+}
