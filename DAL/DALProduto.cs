8c88873b819f32c67aedfb29b787db6c36c3715b
Bueno-Italo <italo-bueno@hotmail.com>
2025-09-02 21:06:05 -0300

    Finalizando metodos da classe DALProduto

--- DAL/DALProduto.cs
+++ DAL/DALProduto.cs
@@ -1,6 +1,7 @@
 ﻿using Modelo;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -46,6 +47,92 @@ namespace DAL
             obj.ProCod = Convert.ToInt32(cmd.ExecuteScalar());
             conexao.Desconectar();
         }
+
+        public void Excluir(int codigo) 
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "delete from Produto where (pro_cod) = (@codigo)";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+        public void Alterar(ModeloProduto obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "UPDATE Produto SET pro_nome = (@nome), pro_descricao = (@descricao), " +
+                "pro_foto = (@foto), pro_valorpago = (@valorpago), pro_valorvenda = (@valorvenda), " +
+                "pro_qtde = (@qtde), umed_cod = (@undmedcod), cat_cod = (@catcod), " +
+                "scat_cod = (@scatcod) WHERE pro_cod = (@codigo) ";
+            cmd.Parameters.AddWithValue("@nome", obj.ProNome);
+            cmd.Parameters.AddWithValue("@descricao", obj.ProDescricao);
+            cmd.Parameters.Add("@foto", System.Data.SqlDbType.Image);
+            if (obj.ProFoto == null)
+            {
+                //cmd.Parameters.AddWithValue("@foto", DBNull.Value);
+                cmd.Parameters["@foto"].Value = DBNull.Value;
+            }
+            else
+            {
+                //cmd.Parameters.AddWithValue("@foto", obj.pro_foto);
+                cmd.Parameters["@foto"].Value = obj.ProFoto;
+            }
+            cmd.Parameters.AddWithValue("@valorpago", obj.ProValorPago);
+            cmd.Parameters.AddWithValue("@valorvenda", obj.ProValorVenda);
+            cmd.Parameters.AddWithValue("@qtde", obj.ProQtde);
+            cmd.Parameters.AddWithValue("@undmedcod", obj.UmedCod);
+            cmd.Parameters.AddWithValue("@catcod", obj.CatCod);
+            cmd.Parameters.AddWithValue("@scatcod", obj.ScatCod);
+            cmd.Parameters.AddWithValue("@codigo", obj.ProCod);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Produto qhere pro_nome like '%" +
+                valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        public ModeloProduto CarregaModeloProduto(int codigo)
+        {
+            ModeloProduto modelo = new ModeloProduto();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "select * from Produto where pro_cod =" + codigo.ToString();
+            conexao.Conectar();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.ProCod = Convert.ToInt32(registro["pro_cod"]);
+                modelo.ProNome = Convert.ToString(registro["pro_nome"]);
+                modelo.ProDescricao = Convert.ToString(registro["pro_descricao"]);
+                try
+                {
+                    modelo.ProFoto = (byte[])registro["pro_foto"];
+
+                }
+                catch { }
+                modelo.ProValorPago = Convert.ToDouble(registro["pro_valorpago"]);
+                modelo.ProValorVenda = Convert.ToDouble(registro["pro_valorvenda"]);
+                modelo.ProQtde = Convert.ToInt32(registro["pro_qtde"]);
+                modelo.UmedCod = Convert.ToInt32(registro["umed_cod"]);
+                modelo.CatCod = Convert.ToInt32(registro["cat_cod"]);
+                modelo.ScatCod = Convert.ToInt32(registro["scat_cod"]);
+            }
+            conexao.Desconectar();
+            return modelo;
+        }
+
+
 
     }
 
